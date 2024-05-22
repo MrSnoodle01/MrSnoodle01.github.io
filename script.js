cfbf4911ce30368af335f3fecd70d3d24342cac3
MrSnoodle01 <richardsonjosh03@gmail.com>
2024-05-22 12:04:01 -0700

    added buttons

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('sorting').addEventListener('click', () =>{
+    console.log("sorting");
+})
+
+document.getElementById('searching').addEventListener('click', () =>{
+    console.log("searching");
+})
+
+document.getElementById('shortest-path').addEventListener('click', () =>{
+    console.log("shortest path");
+})
