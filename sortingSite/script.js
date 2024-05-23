6a5b70a8574b28b324f7d08c1599c5173cd820ed
MrSnoodle01 <richardsonjosh03@gmail.com>
2024-05-22 22:28:02 -0700

    added max number for item input

--- sortingSite/script.js
+++ sortingSite/script.js
@@ -9,10 +9,17 @@ document.getElementById('home-button').addEventListener('click', () =>{
 document.getElementById('submit').addEventListener('click', () =>{
     let numItems = document.getElementById("num-items").value;
 
+    // make sure user's number is at most 100
+    if(numItems > 100){
+        alert("please enter a number less than 100");
+        return;
+    }
+
     // fill array with random values
     let numArray = []
     for(let i = 0; i < numItems; i++){
         numArray.push(Math.floor(Math.random()*MAX));
         console.log(numArray[i]);
     }
+    console.log(numItems);
 })
