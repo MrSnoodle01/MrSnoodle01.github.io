149b18870f63dbf1d63bb547a9af25138de1f599
MrSnoodle01 <richardsonjosh03@gmail.com>
2024-05-31 11:18:25 -0700

    now using canvas element. Primitive connection drawing system

--- shortestPathSite/script.js
+++ shortestPathSite/script.js
@@ -2,7 +2,8 @@ const MAX = 20;
 var delay = 0;
 var nodeArr = [];
 var connectionsArr = [];
-var container = document.getElementById("array");
+// var container = document.getElementById("array");
+const canvas = document.getElementById("canvas");
 
 // connection class to connect nodes
 class connection{
@@ -10,6 +11,14 @@ class connection{
         this.start = start;
         this.weight = weight;
         this.end = end;
+    }
+}
+
+class node{
+    constructor(x, y, value){
+        this.x = x;
+        this.y = y;
+        this.value = value;
     }
 }
 
@@ -41,7 +50,13 @@ document.getElementById('submit').addEventListener('click', () =>{
     delay = (delay - 250) * (-1);
 
     // clear old bars
-    document.getElementById("array").innerHTML = "";
+    // document.getElementById("array").innerHTML = "";
+    
+    // clear old nodes
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    connectionsArr = [];
+    nodeArr = [];
 
     getConnections();
     generateArray(numItems);
@@ -73,48 +88,37 @@ function getConnections(){
     let input = document.getElementById('customText').value;
     let j = 0;
     for(let i = 0; i < input.length; i+=6){
+        // TODO: fix so that weight can work with more than just single digit numbers
         let tempObj = new connection(input[i], input[i+2], input[i+4]);
-        connectionsArr[j] = tempObj;
+        connectionsArr.push(tempObj);
         j++;
-    }
-    for(let i = 0; i < connectionsArr.length; i++){
     }
 }
 
-// generate array of nodes and connects them
+//generate array of nodes and connects them
 function generateArray(numItems){
-    let characters = 'SABCDEFGHIJKLMNOPQRTUVWXYZ'
+    const context = canvas.getContext("2d");
+    let characters = 'SABCDEFGHIJKLMNOPQRTUVWXYZ';
     // get square root of closest even square root of numItems to make nice square with nodes
     let offsetVal = Math.sqrt(Math.pow(Math.ceil(Math.sqrt(numItems)),2));
     let offsetX = 0;
     let offsetY = 0;
-    let backgroundHeight = document.getElementById('array').offsetHeight;
-    let backgroundWidth = document.getElementById('array').offsetWidth;
+    let backgroundHeight = canvas.height;
+    let backgroundWidth = canvas.width;
+    let width = canvas.width*.1;
+    let height = canvas.height*.1;
     for(let i = 0; i < numItems; i++){
+        let x = parseInt(backgroundWidth*offsetY);
+        let y = parseInt(backgroundHeight*offsetX);
         let value = characters.charAt(i);
-        nodeArr[i] = value;
+        let tempObj = new node(x, y, value);
+        nodeArr.push(tempObj);
 
-        // create element div
-        let array_ele = document.createElement("div");
-
-        // add unique id to div
-        array_ele.id = `node ${value}`;
-
-        // add class node to div
-        array_ele.classList.add("node");
-
-        // move nodes to correct location
-        // TODO: MAKE THIS DYNAMICALLY WORK WHEN CHANGING SCREEN SIZES
-        array_ele.style.transform = `translate(${backgroundWidth*offsetX}px, ${backgroundHeight*offsetY}px)`;
-
-        // creating label element for displaying node identifier
-        let array_ele_label = document.createElement("label");
-        array_ele_label.classList.add("node_id");
-        array_ele_label.innerText = value;
-
-        // appending created elements to index.html
-        array_ele.appendChild(array_ele_label);
-        container.appendChild(array_ele);
+        // add nodes to canvas
+        context.fillStyle = "white";
+        context.fillRect(x, y, width, height);
+        context.fillStyle = "black";
+        context.fillText(value, x+(width/2), y+(height/2));
 
         if((i+1) % offsetVal == 0){
             offsetY += 1/(offsetVal - .5);
@@ -124,21 +128,27 @@ function generateArray(numItems){
         }
     }
 
+    console.log(connectionsArr.length);
+    console.log(nodeArr.length);
     // add connections to nodes
     for(let i = 0; i < connectionsArr.length; i++){
-        let start = document.getElementById(`node ${connectionsArr[i].start}`);
-        let startPos = start.getBoundingClientRect();
-        let startX = startPos.left;
-        let startY = startPos.top;
-        console.log(startX, startY);
-
-        let end = document.getElementById(`node ${connectionsArr[i].end}`);
-        let endPos = end.getBoundingClientRect();
-        let endX = endPos.left;
-        let endY = endPos.top;
-
-        let line = document.getElementById('array');s
-        line.innerHTML += `<svg><line x1="${startX}" y1="${startY}" x2="${endX}" y2="${endY}" stroke="white"/></svg>`;
+        // get start and end coordinates
+        let startX = 0, startY = 0, endX = 0, endY = 0;
+        for(let j = 0; j < nodeArr.length; j++){
+            if(nodeArr[j].value == connectionsArr[i].start){
+                startX = nodeArr[j].x;
+                startY = nodeArr[j].y;
+            }
+            if(nodeArr[j].value == connectionsArr[i].end){
+                endX = nodeArr[j].x;
+                endY = nodeArr[j].y;
+            }
+        }
+        context.strokeStyle = "white";
+        context.beginPath();
+        context.moveTo(startX+(width/2), startY+(height/2));
+        context.lineTo(endX+(width/2), endY+(height/2));
+        context.stroke();
     }
 }
 
