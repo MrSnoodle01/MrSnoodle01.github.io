597c512805ee3af6ce1e917e6301ce76c782ac36
Josh Richardson <richardsonjosh03@gmail.com>
2025-08-10 12:02:59 -0700

    replacing old website with new react one

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <link rel="icon" type="image/svg+xml" href="./src/assets/waving-hand.svg" />
+      <h1>👋 Hey there, Welcome to my website</h1>
+      <p className="random-links">
+        <a href="https://github.com/MrSnoodle01">Github</a>
+        <a href="https://www.mountainproject.com/user/201304798/josh-richardson">Mountain Project</a>
+      </p>
+      <a href="https://mrsnoodle01.github.io/Algorithms/">Visit my algorithms site!</a>
+    </>
+  )
+}
+
+export default App
