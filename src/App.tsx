d0cd57015cf02015c5f9ff04748e371ca27e2d61
Josh Richardson <richardsonjosh03@gmail.com>
2025-08-11 16:30:37 -0700

    added about me and climbing sections

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,18 @@ function App() {
       <h1>👋 Hey there, welcome to my website</h1>
       <p className="random-links">
         <a href="https://github.com/MrSnoodle01">Github</a>
+        <a href="https://www.linkedin.com/in/joshua-richardson-084860284/">Linkedin</a>
         <a href="https://www.mountainproject.com/user/201304798/josh-richardson">Mountain Project</a>
       </p>
+      <h2>👀 About me</h2>
       <p style={{ maxWidth: "650px" }}>
-        I’m Josh, a software developer with a passion for creating engaging and interactive web applications.
-        This site showcases some of my projects, including an algorithm visualizer and a Unity game.
+        I’m Josh, a software developer with a passion for learning about computer science.
+        This site showcases some of my projects, including an algorithm visualizer and a Unity game. Check them out below!
+      </p>
+      <h2>🧗‍♂️ Climbing</h2>
+      <p style={{ maxWidth: "650px" }}>
+        I love climbing, especially bouldering and trad climbing. I enjoy the challenge of solving problems and pushing my limits.
+        You can find some of my climbing achievements on my <a href="https://www.mountainproject.com/user/201304798/josh-richardson">Mountain Project</a> account.
       </p>
       <ProjectList />
     </>
