7937a2969f4239778eb5ef820cfc71cadf54908d
MrSnoodle01 <richardsonjosh03@gmail.com>
2025-08-28 17:27:54 -0700

    changed to hash routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { HashRouter, Routes, Route } from 'react-router'
 import './index.css'
 import App from './App.tsx'
 import Projects from './pages/Projects.tsx'
 import Climbing from './pages/Climbing.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App title="Home" />} />
       <Route path="/Projects" element={<Projects title="Projects" />} />
       <Route path="/Climbing" element={<Climbing title="Climbing" />} />
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
 )
